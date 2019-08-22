ab249880bc79a2db80a0288a5b3177294e1cb2cb
radisavljevic <ropeta011@gmail.com>
2019-08-22 12:51:38 +0200

    3rd commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
 ReactDOM.render(<App />, document.getElementById('root'));
+//Jos jedan dodatak
 
 //Brisanje i dodavanje commetnara
 serviceWorker.unregister();
